035708c654b4b779bfd458165d2ccfcc41ec0ac3
MdKaif2782 <96629554+MdKaif2782@users.noreply.github.com>
2022-01-26 18:16:33 +0600

    JavaCord Is Government

--- src/main/java/Banking/sql/robbing.java
+++ src/main/java/Banking/sql/robbing.java
@@ -10,7 +10,7 @@ public class robbing implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         String[] msg = event.getMessageContent().split(" ");
-        if (msg[0].equalsIgnoreCase("!rob") && msg.length<3){
+        if (msg[0].equalsIgnoreCase("!rob") && msg.length<3) {
 
             long victimUserID = event.getMessage().getMentionedUsers().get(0).getId();
             long robbberID = event.getMessageAuthor().getId();
@@ -18,83 +18,89 @@ public class robbing implements MessageCreateListener {
 
             Random random = new Random();
             int ran = random.nextInt(100);
+            if (!event.getMessage().getMentionedUsers().get(0).isBot()) {
+                if (ran < 50) {
 
-            if (ran<50){
+                    try {
+                        Connection conn = DriverManager.getConnection("jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3467490?" +
+                                "user=sql3467490&password=a7leh3zlaA");
+                        Statement stmt = conn.createStatement();
+                        ResultSet resultSet = stmt.executeQuery("select * from MainTable " + " where ServerID=" + ServerID + " AND UserID=" + victimUserID);
 
-                try {
-                    Connection conn = DriverManager.getConnection("jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3467490?" +
-                            "user=sql3467490&password=a7leh3zlaA");
-                    Statement stmt=conn.createStatement();
-                    ResultSet resultSet = stmt.executeQuery("select * from MainTable "+" where ServerID="+ServerID+" AND UserID="+victimUserID);
+                        String moneyofvictim = "0";
+                        while (resultSet.next()) {
+                            moneyofvictim = resultSet.getString("Money");
+                        }
+                        long moneyV = Long.parseLong(moneyofvictim);
 
-                    String moneyofvictim = "0";
-                    while (resultSet.next()){
-                        moneyofvictim = resultSet.getString("Money");
+                        if (moneyV > 100) {
+                            long newMoneyofVictim = (long) (moneyV - moneyV * 0.10);
+                            long addedbalancetorobber = (long) (moneyV * 0.10);
+
+                            PreparedStatement statement = conn.prepareStatement("UPDATE MainTable " +
+                                    " SET Money=Money-" + addedbalancetorobber + ", TotalMoney=TotalMoney-" + addedbalancetorobber +
+                                    " WHERE ServerID=" + ServerID + " AND UserID=" + victimUserID);
+                            statement.executeUpdate();
+
+                            PreparedStatement statement2 = conn.prepareStatement("UPDATE MainTable " +
+                                    " SET Money=Money+" + addedbalancetorobber + ", TotalMoney=TotalMoney+" + addedbalancetorobber +
+                                    " WHERE ServerID=" + ServerID + " AND UserID=" + robbberID);
+                            statement2.executeUpdate();
+
+
+                            event.getChannel().sendMessage("You robbed " + addedbalancetorobber + "$ from" + event.getMessage().getMentionedUsers().get(0).getMentionTag());
+
+
+                        } else {
+                            event.getChannel().sendMessage("you cant rob from poors");
+                        }
+
+                        conn.close();
+
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-                    long moneyV = Long.parseLong(moneyofvictim);
 
-                    if (moneyV>100){
-                        long newMoneyofVictim = (long) (moneyV-moneyV*0.10);
-                        long addedbalancetorobber = (long) (moneyV*0.10);
+                } else {
+
+                    try {
+                        Connection conn = DriverManager.getConnection("jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3467490?" +
+                                "user=sql3467490&password=a7leh3zlaA");
+                        Statement stmt = conn.createStatement();
+                        ResultSet resultSet = stmt.executeQuery("select * from MainTable " + " where ServerID=" + ServerID + " AND UserID=" + robbberID);
+                        String robbermoney = "0";
+                        while (resultSet.next()) {
+                            robbermoney = resultSet.getString("Money");
+                        }
+                        long robberM = Long.parseLong(robbermoney);
+                        long policefine = (long) (robberM * 0.12);
 
                         PreparedStatement statement = conn.prepareStatement("UPDATE MainTable " +
-                                " SET Money=Money-"+addedbalancetorobber+ ", TotalMoney=TotalMoney-"+addedbalancetorobber+
-                                " WHERE ServerID="+ServerID +" AND UserID="+victimUserID);
+                                " SET Money=Money-" + policefine + ", TotalMoney=TotalMoney-" + policefine +
+                                " WHERE ServerID=" + ServerID + " AND UserID=" + robbberID);
                         statement.executeUpdate();
 
                         PreparedStatement statement2 = conn.prepareStatement("UPDATE MainTable " +
-                                " SET Money=Money+"+addedbalancetorobber+", TotalMoney=TotalMoney+"+addedbalancetorobber+
-                                " WHERE ServerID="+ServerID +" AND UserID="+robbberID);
+                                " SET Money=Money+" + policefine + ", TotalMoney=TotalMoney+" + policefine +
+                                " WHERE ServerID=" + ServerID + " AND UserID=924608474409742357");
                         statement2.executeUpdate();
 
+                        conn.close();
 
-                        event.getChannel().sendMessage("You robbed " + addedbalancetorobber+"$ from"+ event.getMessage().getMentionedUsers().get(0).getMentionTag());
+                        event.getChannel().sendMessage("You got cought by police while robbing " + event.getMessage().getMentionedUsers().get(0).getMentionTag() + "" +
+                                "\nYou had to bribe police " + policefine + "$ to escape\n" +
+                                "What a pathetic looser you are hghghghghghg");
 
-
-                    } else {
-                        event.getChannel().sendMessage("you cant rob from poors");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
 
-                    conn.close();
-
-
-
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
                 }
 
             } else {
-
-                try {
-                    Connection conn = DriverManager.getConnection("jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3467490?" +
-                            "user=sql3467490&password=a7leh3zlaA");
-                    Statement stmt=conn.createStatement();
-                    ResultSet resultSet = stmt.executeQuery("select * from MainTable "+" where ServerID="+ServerID+" AND UserID="+robbberID);
-                    String robbermoney = "0";
-                    while (resultSet.next()){
-                        robbermoney = resultSet.getString("Money");
-                    }
-                    long robberM = Long.parseLong(robbermoney);
-                    long policefine = (long) (robberM*0.12);
-
-                    PreparedStatement statement = conn.prepareStatement("UPDATE MainTable " +
-                            " SET Money=Money-"+policefine+ ", TotalMoney=TotalMoney-"+policefine+
-                            " WHERE ServerID="+ServerID +" AND UserID="+robbberID);
-                    statement.executeUpdate();
-
-                    conn.close();
-
-                    event.getChannel().sendMessage("You got cought by police while robbing "+ event.getMessage().getMentionedUsers().get(0).getMentionTag() +"" +
-                            "\nYou had to bribe police "+policefine+"$ to escape\n" +
-                            "What a pathetic looser you are hghghghghghg");
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
+                event.getChannel().sendMessage("You can't rob bots");
             }
-
         }
     }
 }
