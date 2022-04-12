d40f157bd65e05afbf6043602b7aba37994686d1
MdKaif2782 <96629554+MdKaif2782@users.noreply.github.com>
2022-04-12 23:55:18 +0600

    nhentai search update **Command:** ```!nhsearch title:mother and no other tag:incest```
    ```!nhsearch tag:incest```
    
    ```!nhsearch title:mother and no other```

--- src/main/java/Commands/APIcommands/nhentai/nQuery.java
+++ src/main/java/Commands/APIcommands/nhentai/nQuery.java
@@ -1,6 +1,7 @@
 package Commands.APIcommands.nhentai;
 
 import net.beardbot.nhentai.NHentai;
+import net.beardbot.nhentai.api.Gallery;
 import net.beardbot.nhentai.api.GalleryPage;
 import net.beardbot.nhentai.api.SearchResult;
 import net.beardbot.nhentai.api.params.Language;
@@ -33,65 +34,63 @@ public class nQuery implements MessageCreateListener {
 
             var result = nHentai.galleries().search(message);
 
-            ArrayList<String> Title = new ArrayList<>();
-            ArrayList<String> Codes = new ArrayList<>();
-            for (int i=0; i<result.getGalleries().size();i++) {
-                Title.add(result.getGalleries().get(i).getEnglishTitle());
-                Codes.add(String.valueOf(result.getGalleries().get(i).getId()));
 
-                System.out.println(result.getGalleries().get(i).getEnglishTitle() + "\n" +
-                        result.getGalleries().get(i).getId() + "\n");
-
-            }
-            System.out.println(result.getGalleries().size()+" results found");
-            if (result.getGalleries().isEmpty()){
-                System.out.println("no result found");
-            }
 
             int num = 0;
+
             AtomicReference<AtomicInteger> j = new AtomicReference<>(new AtomicInteger());
+            AtomicReference<AtomicInteger> pageno = new AtomicReference<>(new AtomicInteger());
+            pageno.get().getAndIncrement();
             String tggg= result.getGalleries().get(num).getTags().get(0).getName()+", ";
             for (int i=0;i<result.getGalleries().get(num).getTags().size();i++){
                 tggg=tggg+result.getGalleries().get(num).getTags().get(i).getName()+", ";
             }
+
             String finalTggg = tggg;
             SearchResult finalResult = result;
             SearchResult finalResult1 = result;
             SearchResult finalResult2 = result;
             SearchResult finalResult3 = result;
             new MessageBuilder().setEmbeds(new EmbedBuilder()
-                            .setTitle(result.getGalleries().get(num).getEnglishTitle())
-                            .setThumbnail(result.getGalleries().get(num).getThumbnail().getDownloadUrl())
-                            .addField("Code","**"+Codes.get(num)+"**")
-                            .addField("Tags",tggg)
-                            .addField("Number of pages", String.valueOf(result.getGalleries().get(num).getPages().size()))
-                            .addField("Favorites", String.valueOf(result.getGalleries().get(num).getNumberOfFavorites()))
-                            .addField("Uploaded",result.getGalleries().get(num).getUploadDate().toString())
+                    .setTitle(result.getGalleries().get(num).getEnglishTitle())
+                    .setThumbnail(result.getGalleries().get(num).getThumbnail().getDownloadUrl())
+                    .addField("Code","**"+result.getGalleries().get(0).getId()+"**")
+                    .addField("Tags",tggg)
+                    .addField("Number of pages", String.valueOf(result.getGalleries().get(num).getPages().size()))
+                    .addField("Favorites", String.valueOf(result.getGalleries().get(num).getNumberOfFavorites()))
+                    .addField("Uploaded",result.getGalleries().get(num).getUploadDate().toString())
                     .setColor(Color.BLACK)).send(event.getChannel()).thenAccept(message1 -> {
-                        message1.addReaction("\u2B05\uFE0F");
-                        message1.addReaction("\u27A1\uFE0F");
+                message1.addReaction("\u2B05\uFE0F");
+                message1.addReaction("\u27A1\uFE0F");
 
 
 
-                        message1.addReactionAddListener(reactionAddEvent -> {
-                           if (reactionAddEvent.getEmoji().equalsEmoji("\u27A1\uFE0F")){
-                               j.get().getAndIncrement();
-                               int k=j.get().get();
-                               String tago= finalResult.getGalleries().get(k).getTags().get(0).getName()+", ";
-                               for (int i = 0; i< finalResult.getGalleries().get(k).getTags().size(); i++){
-                                   tago=tago+ finalResult.getGalleries().get(k).getTags().get(i).getName()+", ";
-                               }
-                               message1.edit(new EmbedBuilder()
-                                       .setTitle(finalResult.getGalleries().get(k).getEnglishTitle())
-                                       .setThumbnail(finalResult.getGalleries().get(k).getThumbnail().getDownloadUrl())
-                                       .addField("Code","**"+Codes.get(k)+"**")
-                                       .addField("Tags", tago)
-                                       .addField("Number of pages", String.valueOf(finalResult.getGalleries().get(k).getPages().size()))
-                                       .addField("Favorites", String.valueOf(finalResult.getGalleries().get(k).getNumberOfFavorites()))
-                                       .addField("Uploaded", finalResult.getGalleries().get(k).getUploadDate().toString())
-                                       .setColor(Color.BLACK));
-                           }
-                        });
+
+                message1.addReactionAddListener(reactionAddEvent -> {
+                    if (reactionAddEvent.getEmoji().equalsEmoji("\u27A1\uFE0F")){
+                        j.get().getAndIncrement();
+                        int k=j.get().get();
+                        if (k%25==0 && k!=0){
+                            pageno.get().getAndIncrement();
+                            j.get().set(0);
+                        }
+                        int page = pageno.get().get();
+
+                        String tago= finalResult.getGalleriesOnPage(page).get(k).getTags().get(0).getName()+", ";
+                        for (int i = 0; i< finalResult.getGalleriesOnPage(page).get(k).getTags().size(); i++){
+                            tago=tago+ finalResult.getGalleriesOnPage(page).get(k).getTags().get(i).getName()+", ";
+                        }
+                        message1.edit(new EmbedBuilder()
+                                .setTitle(finalResult.getGalleriesOnPage(page).get(k).getEnglishTitle())
+                                .setThumbnail(finalResult.getGalleriesOnPage(page).get(k).getThumbnail().getDownloadUrl())
+                                .addField("Code","**"+result.getGalleriesOnPage(page).get(k).getId()+"**")
+                                .addField("Tags", tago)
+                                .addField("Number of pages", String.valueOf(finalResult.getGalleriesOnPage(page).get(k).getPages().size()))
+                                .addField("Favorites", String.valueOf(finalResult.getGalleriesOnPage(page).get(k).getNumberOfFavorites()))
+                                .addField("Uploaded", finalResult.getGalleriesOnPage(page).get(k).getUploadDate().toString())
+                                .setColor(Color.BLACK));
+                    }
+                });
 
                 message1.addReactionRemoveListener(reactionAddEvent -> {
                     if (reactionAddEvent.getEmoji().equalsEmoji("\u27A1\uFE0F")){
@@ -104,7 +103,7 @@ public class nQuery implements MessageCreateListener {
                         message1.edit(new EmbedBuilder()
                                 .setTitle(finalResult1.getGalleries().get(k).getEnglishTitle())
                                 .setThumbnail(finalResult1.getGalleries().get(k).getThumbnail().getDownloadUrl())
-                                .addField("Code","**"+Codes.get(k)+"**")
+                                .addField("Code","**"+result.getGalleries().get(k).getId()+"**")
                                 .addField("Tags", tago)
                                 .addField("Number of pages", String.valueOf(finalResult1.getGalleries().get(k).getPages().size()))
                                 .addField("Favorites", String.valueOf(finalResult1.getGalleries().get(k).getNumberOfFavorites()))
@@ -123,11 +122,12 @@ public class nQuery implements MessageCreateListener {
                         message1.edit(new EmbedBuilder()
                                 .setTitle(finalResult2.getGalleries().get(k).getEnglishTitle())
                                 .setThumbnail(finalResult2.getGalleries().get(k).getThumbnail().getDownloadUrl())
-                                .addField("Code","**"+Codes.get(k)+"**")
+                                .addField("Code","**"+result.getGalleries().get(k).getId()+"**")
                                 .addField("Tags", tago)
                                 .addField("Number of pages", String.valueOf(finalResult2.getGalleries().get(k).getPages().size()))
                                 .addField("Favorites", String.valueOf(finalResult2.getGalleries().get(k).getNumberOfFavorites()))
                                 .addField("Uploaded", finalResult2.getGalleries().get(k).getUploadDate().toString())
+                                        .setFooter("page "+pageno.get().get()+" item no "+(k+1))
                                 .setColor(Color.BLACK));
                     }
                 });
@@ -142,7 +142,7 @@ public class nQuery implements MessageCreateListener {
                         message1.edit(new EmbedBuilder()
                                 .setTitle(finalResult3.getGalleries().get(k).getEnglishTitle())
                                 .setThumbnail(finalResult3.getGalleries().get(k).getThumbnail().getDownloadUrl())
-                                .addField("Code","**"+Codes.get(k)+"**")
+                                .addField("Code","**"+result.getGalleries().get(k).getId()+"**")
                                 .addField("Tags", tago)
                                 .addField("Number of pages", String.valueOf(finalResult3.getGalleries().get(k).getPages().size()))
                                 .addField("Favorites", String.valueOf(finalResult3.getGalleries().get(k).getNumberOfFavorites()))
